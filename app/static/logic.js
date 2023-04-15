2b47123af7e630368c68ac2ac17e4e82027a0f0f
dkurbel <dkurbel@icloud.com>
2023-04-15 02:43:26 -0500

    web interface design

--- app/static/logic.js
+++ app/static/logic.js
@@ -1,7 +1,6 @@
 //---------------- Construct Model Function
 
 // Load the serialized logistic regression model
-const model = ml5.logisticRegression('../logistic_regression_model.pkl', modelLoaded);
 
 // Callback function to handle when the model is loaded
 function modelLoaded() {
@@ -9,25 +8,38 @@ function modelLoaded() {
   // You can now use the 'model' object to make predictions
 }
 
-//---------------- Submission Form Function
+//---------------- Submission Form Functions
+
+// Retrieve input data from input field
+var question1Input = document.getElementById('question1').value;
+// Store input data in a JavaScript object
+var inputData = {
+  question1: question1Input,
+  // Add more questions as needed
+};
 
 // Event listener for form submission or UI element click
 document.getElementById('submitBtn').addEventListener('click', function() {
-    // Get input data from form or UI elements
-    var inputData = { /* Retrieve input data */ };
-    // Make a POST request to Flask backend with input data
-    axios.post('/predict', inputData)
-        .then(function(response) {
-            // Handle response from Flask backend
-            var predictions = response.data;
-            // Update front-end UI with predictions
-            /* Update UI with predictions */
-        })
-        .catch(function(error) {
-            // Handle error
-            console.error(error);
-        });
+  // Get input data from form
+  var inputData = {
+      question1: document.getElementById('question1').value,
+      question2: document.getElementById('question2').value,
+      // Retrieve values from other input fields as needed
+  };
+  // Make a POST request to Flask backend with input data
+  axios.post('/predict', inputData)
+      .then(function(response) {
+          // Handle response from Flask backend
+          var predictions = response.data;
+          // Update front-end UI with predictions
+          /* Update UI with predictions */
+      })
+      .catch(function(error) {
+          // Handle error
+          console.error(error);
+      });
 });
+
 
 //---------------- Example Data
 
