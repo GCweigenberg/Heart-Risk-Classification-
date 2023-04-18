83eaeb0caff1f673ff98345e3150f87133c1d42c
dkurbel <dkurbel@icloud.com>
2023-04-17 19:28:57 -0500

    app working updates

--- app/app.py
+++ app/app.py
@@ -24,22 +24,53 @@ def predict():
     # Extract values from input_data
     question1 = input_data.get('question1', '')
     question2 = input_data.get('question2', '')
+    question3 = input_data.get('question3', '')
+    question4 = input_data.get('question4', '')
+    question5 = input_data.get('question5', '')
+    question6 = input_data.get('question6', '')
+    question7 = input_data.get('question7', '')
+    question8 = input_data.get('question8', '')
+    question9 = input_data.get('question9', '')
+    question10 = input_data.get('question10', '')
+    question11 = input_data.get('question11', '')
+    question12 = input_data.get('question12', '')
+    question13 = input_data.get('question13', '')
+    question14 = input_data.get('question14', '')
+    question15 = input_data.get('question15', '')
+
     # Extract values from other input fields as needed
 
+    # Preprocess the input data
+    input_df = pd.DataFrame({
+        'question1': [question1],
+        'question2': [question2],
+        'question3': [question3],
+        'question4': [question4],
+        'question5': [question5],
+        'question6': [question6],
+        'question7': [question7],
+        'question8': [question8],
+        'question9': [question9],
+        'question10': [question10],
+        'question11': [question11],
+        'question12': [question12],
+        'question13': [question13],
+        'question14': [question14],
+        'question15': [question15],
+        # Add columns for other input fields as needed
+    })
+    input_df = pd.get_dummies(input_df, drop_first=True)
+    input_scaled = scaler.transform(input_df)
+
     # Perform prediction using the loaded model
-    # Modify this part based on your actual model prediction logic
-    prediction = model.predict([[question1, question2]])
-    # Modify this part based on your actual model prediction logic
+    prediction = model.predict(input_scaled)
 
     # Prepare response
     response = {
         'prediction': prediction.tolist()
     }
 
-    # Return JSON response
-    return jsonify(response)
-
-
+    return response
 
 #If name is main, run flask
 if __name__ == '__main__':
